347bc3118c41d8153d65ae4a84455c1fd4d40b74
LectricAvenue <colin@prospre.io>
2025-11-04 15:03:39 -0330

    fix bad indenting due, add packages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
     vim.filetype.add({ extension = { tsx = "typescriptreact" } })
-
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
-        "lua",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "yaml",
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "html",
-        "css",
-        "regex",
-        "python",
-	"lua"
+        "lua","javascript","typescript","tsx","json","yaml",
+        "bash","markdown","markdown_inline","html","css","regex","python",
       },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-        disable = { "tsx" },
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          node_incremental = "<CR>",
-          scope_incremental = "<S-CR>",
-          node_decremental = "<BS>",
-        },
-      },
+      auto_install = true,
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      indent = { enable = false },
     })
   end,
 }
